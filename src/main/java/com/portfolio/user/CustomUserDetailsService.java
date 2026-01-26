b13d45b867771417d8229a8ef43f91e1093a6988
kamiya-daiki <kamiya.d02@gmail.com>
2026-01-26 15:08:03 +0900

    - データベースを参照してログインするように修正 - htmlファイルの配置ミスを修正 - htmlファイルをログイン結果ごとに追加 - applicationファイルをyamlからpropertiesに変更

---
+++ src/main/java/com/portfolio/user/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.portfolio.user;
+
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        UserEntity user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                    new UsernameNotFoundException("User not found"));
+
+        if (!Boolean.TRUE.equals(user.getEnabled())) {
+            throw new DisabledException("User disabled");
+        }
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword()) // BCrypt hash
+                .roles("USER")
+                .build();
+    }
+}
