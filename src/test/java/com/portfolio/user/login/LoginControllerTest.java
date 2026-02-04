8a9254fe0993ca86143dcaf60c5f6abe01e8175f
kamiya-daiki <kamiya.d02@gmail.com>
2026-02-04 15:08:43 +0900

    - JUnit実行構成(JaCoCo.exec、カバレッジレポート出力まで)

---
+++ src/test/java/com/portfolio/user/login/LoginControllerTest.java
@@ -0,0 +1,33 @@
+package com.portfolio.user.login;
+
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk());
+    }
+
+    // @Autowired
+    // MockMvc mockMvc;
+
+    // @Test
+    // void login画面が表示される() throws Exception {
+    //     mockMvc.perform(get("/login"))
+    //            .andExpect(status().isOk());
+    // }
+}
